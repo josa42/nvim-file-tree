93c3f23bfabd4ae69e4b7ffc0f9808aafb6ba892
Josa Gesell <josa@gesell.me>
2021-12-22 14:47:32 +0100

    feat: add watcher

---
+++ lua/jg/file-tree/fs/watcher.lua
@@ -0,0 +1,25 @@
+local uv = vim.loop
+
+-- TODO remove plenary dependency
+local void = require('plenary.async.async').void
+local scheduler = require('plenary.async.util').scheduler
+
+local M = {}
+
+function M.watch(dir, fn, opts)
+  opts = opts or {}
+
+  local w = uv.new_fs_poll()
+  w:start(
+    dir,
+    opts.interval or 1000,
+    void(function()
+      scheduler()
+      fn()
+    end)
+  )
+
+  return w
+end
+
+return M
