c0e6c5f5570ad426f46afc847fc98310a899ce22
Josa Gesell <josa@gesell.me>
2021-12-24 17:55:40 +0100

    chore: Disable logging

--- lua/file-tree/logger.lua
+++ lua/file-tree/logger.lua
@@ -1,8 +1,13 @@
 local M = {}
 
+M.enabled = false
 M.file_path = os.getenv('HOME') .. '/tmp/log/file-tree.log'
 
 function M.log(msg)
+  if not M.enabled then
+    return
+  end
+
   local fp = assert(io.open(M.file_path, 'a'))
   local str = string.format('[%-6s%s] %s\n', 'LOG', os.date(), msg)
   fp:write(str)
